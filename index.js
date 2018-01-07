211c77740a2890ab6f6619a353eff02ba465ea7c
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-07 20:37:10 +0200

    poisto toimii

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 
-const persons = [
+let persons = [
     {
       "name": "Arto Hellas",
       "number": "040-123456",
@@ -44,6 +44,12 @@ app.get('/api/persons', (req, res) => {
     } else {
       response.status(404).end()
     }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
   })
   
   const PORT = 3001
