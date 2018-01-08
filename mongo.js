bf691c1223babe4017382ffc6c835bce540e5ed1
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-08 22:24:01 +0200

    if no arguments, list is printed

--- mongo.js
+++ mongo.js
@@ -5,22 +5,38 @@ const url = 'mongodb://user:password@ds247327.mlab.com:47327/sandbox'
 mongoose.connect(url, { useMongoClient: true })
 mongoose.Promise = global.Promise;
 
-const Person = mongoose.model('Note', {
+const Person = mongoose.model('Person', {
     name: String,
     number: String
   })
 
-  const name = process.argv[2]
-  const number = process.argv[3];
+  if (process.argv.length === 2) {
+      console.log("puhelinluettelo")
+    Person
+    .find({})
+    .then(result => {
+      result.forEach(person => {
+        console.log(person.name + " " + person.number)
+      })
+      mongoose.connection.close()
+    })
+  }
+  else if (process.argv.length === 4) {
+    const name = process.argv[2]
+    const number = process.argv[3];
+  
+    const person = new Person({
+        name: name,
+        number: number
+    })
+  
+  
+  
+    person.save().then(response => {
+        console.log("lisätään henkilö " + person.name + " " + person.number + " luetteloon")
+        mongoose.connection.close();
+    })
+  }
 
-  const person = new Person({
-      name: name,
-      number: number
-  })
-
-  person.save().then(response => {
-      console.log("person saved to mongoDB")
-      mongoose.connection.close();
-  })
 
 
