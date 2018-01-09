a3f820d797d31eb04422d1c6f604941ed197e805
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-09 17:43:13 +0200

    using enviroment variable to determine database

--- models/person.js
+++ models/person.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 if ( process.env.NODE_ENV !== 'production' ) {
   require('dotenv').config()
 }
-
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useMongoClient: true })
